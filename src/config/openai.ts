6a7e33cd15050180e0a9bbbc3006365e405295dc
Stefan Wallin <github@stefan-wallin.se>
2024-05-21 11:40:57 +0200

    Add config file

---
+++ src/config/openai.ts
@@ -0,0 +1,7 @@
+export default {
+  token: process.env.DISCORD_TOKEN,
+  clientId: process.env.DISCORD_APPLICATION_ID,
+  guildId: process.env.DISCORD_SERVER_ID,
+  channelId: process.env.DISCORD_CHANNEL_ID,
+  botUserId: process.env.DISCORD_BOT_USER_ID
+}
